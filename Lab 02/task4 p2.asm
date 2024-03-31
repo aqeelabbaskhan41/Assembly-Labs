d69434b3c908ca34c7fe3e6b110394d18185a2dd
aqeelabbaskhan41 <156291160+aqeelabbaskhan41@users.noreply.github.com>
2024-03-31 08:17:24 +0500

    Add files via upload

---
+++ Lab 02/task4 p2.asm
@@ -0,0 +1,22 @@
+INCLUDE Irvine32.inc
+
+.data
+    var1 DWORD 5h
+    var2 DWORD 6h
+    var3 DWORD 2h
+    result DWORD ?
+
+.code
+main PROC
+    
+    ; given expression 5+(6-2) 
+    mov eax, var2	; moving value of var2 into eax
+    sub eax, var3	; substracting value of var3 from eax
+    mov ebx, var1
+    add eax, ebx	; adding value of var1 into eax
+    mov result, eax	; storing the result of expression into result variable
+    
+    call DumpRegs	; displaying the result
+    exit
+main ENDP
+END main
